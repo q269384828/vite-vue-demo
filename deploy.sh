59a7ca690c74f7b974931167c6c3976fe338eb4e
zhangsan <zhagsan@qq.com>
2024-12-03 10:08:12 +0800

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入构建文件夹
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:q269384828/vite-vue-demo.git main:gh-pages
+cd -
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'vite-vue-demo',
   plugins: [vue(), VueJsx()],
 })
