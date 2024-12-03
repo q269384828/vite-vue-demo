64ca61f9f7ecb6323df430dc9ec315bce6544f9c
zhangsan <zhagsan@qq.com>
2024-12-03 10:27:48 +0800

    q

--- deploy.mjs
+++ deploy.mjs
@@ -2,7 +2,7 @@
 import ghpages from 'gh-pages'
 
 ghpages.publish('dist', {
-  branch: 'main',
+  branch: 'gh-pages',
   repo: 'https://github.com/q269384828/vite-vue-demo.git',
 }, (e) => {
   console.log('success', e)
